81e3f8464d63fc9d8555d578db6f2591aba93ef7
Marmik198 <shahmarmik1@gmail.com>
2022-06-16 12:40:26 +0530

    Added NavBar Component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav>
+    <v-app-bar flat app left dark class="text-uppercase">
+      <h1 class="brand"><i class="fas fa-clock mr-2"></i></h1>
+      <h1 class="font-weight-light brand">World</h1>
+      <h1 class="brand">Clock</h1>
+      <v-spacer></v-spacer>
+      <v-btn depressed class="blue-grey d-none d-sm-flex">
+        <a
+          class="nav-link"
+          href="https://gent-2ithub.com/Marmik198"
+          target="_blank"
+        >
+          <span>Marmik Shah</span>
+          <i class="fas fa-user ml-2" right></i>
+        </a>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  methods: {},
+};
+</script>
+
+<style scoped>
+.nav-link {
+  text-decoration: none;
+  color: #fff;
+  transition: 0.5s ease-in;
+}
+
+.nav-link:hover {
+  text-decoration: underline;
+}
+
+.brand {
+  letter-spacing: 3px;
+}
+</style>
